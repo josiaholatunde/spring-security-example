36f5fd127a2cef59ffcbc84722fb35cc0c15c5d9
Ogunboyejo Olatunde <oogunnboyejo@teamapt.com>
2021-04-24 13:58:58 +0100

    fix

--- src/main/java/com/olatunde/conferencesecurity/controllers/AuthenticationController.java
+++ src/main/java/com/olatunde/conferencesecurity/controllers/AuthenticationController.java
@@ -2,6 +2,7 @@ package com.olatunde.conferencesecurity.controllers;
 
 import com.olatunde.conferencesecurity.dtos.request.UserRegistrationRequestDto;
 import com.olatunde.conferencesecurity.dtos.response.UserRegistrationResponseDto;
+import com.olatunde.conferencesecurity.services.AccountConfirmationService;
 import com.olatunde.conferencesecurity.services.AccountRegistrationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 public class AuthenticationController {
 
     private final AccountRegistrationService accountRegistrationService;
+    private final AccountConfirmationService accountConfirmationService;
 
     @PostMapping("/register")
     public UserRegistrationResponseDto register(@RequestBody @Validated UserRegistrationRequestDto userRegistrationDto, BindingResult bindingResult) {
@@ -27,8 +29,8 @@ public class AuthenticationController {
     }
 
     @GetMapping("account-confirmation")
-    public void accountConfirmation(@RequestParam(name = "token") String token) {
-
+    public void accountConfirmation(@RequestParam(name = "token") String token) throws Exception {
+         accountConfirmationService.confirmAccount(token);
     }
 
 
